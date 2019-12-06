026d8d0dda914a2f9efbd353940b73b172cbdd96
Noor <noordotkhan@gmail.com>
2019-12-06 13:53:08 -0500

    Finished q3 p2

--- 3.clj
+++ 3.clj
@@ -2,6 +2,7 @@
 (def test-input2 (clojure.string/split "U7,R6,D4,L4" #","))
 
 (def input (map #(clojure.string/split % #",") (clojure.string/split-lines (slurp (clojure.java.io/resource "input3")))))
+(def garret-input (map #(clojure.string/split % #",") (clojure.string/split-lines (slurp (clojure.java.io/resource "garretinput3")))))
 
 (defn get-points
   ([path]
@@ -55,8 +56,8 @@
 
 (defn get-min-intersection-distance [path1 path2]
   (apply min (map #(apply + %) (reduce #(conj %1 (map (fn [n] (Math/abs n)) %2))
-                             #{}
-                             (clojure.set/intersection (get-points path1) (get-points path2))))))
+                                       #{}
+                                       (clojure.set/intersection (get-points path1) (get-points path2))))))
 
 (get-min-intersection-distance test-input1 test-input2) ;; 6 is correct
 
@@ -70,4 +71,67 @@
 
 (get-min-intersection-distance test-input5 test-input6) ;; 135 is correct
 
-(get-min-intersection-distance (nth input 0) (nth input 1)) ;; part 1
+(def input-path1 (nth input 0))
+(def input-path2 (nth input 1))
+
+(get-min-intersection-distance input-path1 input-path2) ;; part 1
+(get-min-intersection-distance (nth garret-input 0) (nth garret-input 1)) ;; garret
+
+(defn get-intersection-points [path1 path2]
+  (vec (clojure.set/intersection (get-points path1) (get-points path2))))
+
+(def intersection-points (get-intersection-points input-path1 input-path2))
+intersection-points
+(get intersection-points 0)
+
+(defn get-point-distance-map
+  [path point]
+  (loop [x 0
+         y 0
+         point-distance-map {}
+         path-step 0
+         current-distance-step 0
+         total-distance 0]
+    (if (= path-step (count path))
+      point-distance-map
+      (let [current-step (get path path-step)
+            current-direction (get current-step 0)
+            distance (Integer/parseInt (subs current-step 1))]
+        (if (and (= [x y] point) (not (contains? point-distance-map point)))
+          (recur x y (assoc point-distance-map point total-distance) path-step
+                 current-distance-step total-distance)
+          (if (= current-distance-step distance)
+            (recur x y point-distance-map (inc path-step) 0 total-distance)
+            (cond (= current-direction \R)
+                  (recur (inc x) y point-distance-map path-step (inc current-distance-step) (inc total-distance))
+                  (= current-direction \U)
+                  (recur x (inc y) point-distance-map path-step (inc current-distance-step) (inc total-distance))
+                  (= current-direction \L)
+                  (recur (dec x) y point-distance-map path-step (inc current-distance-step) (inc total-distance))
+                  (= current-direction \D)
+                  (recur x (dec y) point-distance-map path-step (inc current-distance-step) (inc total-distance)))))))))
+
+(get-point-distance-map input-path1 (get intersection-points 10))
+
+intersection-points
+(reduce #(merge %1 (get-point-distance-map input-path1 %2)) {} intersection-points)
+
+(get-intersection-points test-input1 test-input2)
+
+(defn get-points-distance-map [path1 points]
+  (reduce #(merge %1 (get-point-distance-map path1 %2)) {} points))
+
+(get-points-distance-map test-input1 (get-intersection-points test-input1 test-input2))
+(get-points-distance-map test-input2 (get-intersection-points test-input1 test-input2))
+
+(defn get-minimum-walked-intersection-distance [path1 path2]
+  (let [intersection-points (get-intersection-points path1 path2)
+        path1-points-distance-map (get-points-distance-map path1 intersection-points)
+        path2-points-distance-map (get-points-distance-map path2 intersection-points)]
+    (apply min (reduce
+                #(conj %1 (+ (get path1-points-distance-map %2) (get path2-points-distance-map %2)))
+                []
+                intersection-points))))
+
+(get-minimum-walked-intersection-distance test-input1 test-input2)
+(get-minimum-walked-intersection-distance input-path1 input-path2)
